37c3012c7ed2154433e3fd3a710cfbbfb8406f81
yuuu.miao@gmail.com <yuuumiao@miyaas.home>
2020-11-24 21:15:01 +0100

    the jump need be more smooth

--- js/main.js
+++ js/main.js
@@ -1,64 +1,12 @@
-
-// import SpriteSheet from './spritesSheet';
-
-class SpriteSheet {
-    constructor(image, width, height) {
-        this.image = image;
-        this.width = width;
-        this.height = height;
-        this.tiles = new Map();
-    }
-
-
-    define(name, x, y) {
-            const buffer = document.createElement('canvas');
-            buffer.width = this.width;
-            buffer.height = this.height;
-
-            buffer
-                .getContext('2d')
-                .drawImage(
-                    this.image,
-                    x * this.width,
-                    y * this.height,
-                    this.width,
-                    this.height,
-                    0,
-                    0,
-                    this.width,
-                    this.height);
-              this.tiles.set(name,buffer)  
-        }
-        
-     draw(name,context,x,y){
-            const buffer = this.tiles.get(name);
-            context.drawImage(buffer,x,y);  
-            }
-
- }
-
-
-
-
-function loadImage(url){
-    return new Promise(resolve => {
-        const image = new Image();
-        image.addEventListener('load', () => {
-            resolve(image);
-         });
-        image.src = url;
-    });
-}
+import {loadImage, SpriteSheet} from './world.js'
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
 
-//context.fillRect(0,0,100,100);
 loadImage('../imgs/tiles_map.png')
     .then (image => {
     const sprites = new SpriteSheet(image,70,70);
-    // sprites.draw('grassGround', context, 40, 40);
     sprites.define('sky',8,0);
 
      for(let x =0; x < 14; x++) {
@@ -75,11 +23,92 @@ loadImage('../imgs/tiles_map.png')
      }
  }
 
+});
 
 
-//    context.drawImage(image,490,0,70,70,
-//                            10,10,50,50);
+const theCat = {
+    position: {
+        x:100,
+        y:470,
+    },
+
+    isJumping: false,
+
+    loadCat(){
+        return loadImage('../imgs/nyanCat.png')
+        .then (image => {
+            const sprites = new SpriteSheet(image, 80,80);
+            sprites.define('idle',0,0);
+            sprites.draw('idle',context, this.position.x,this.position.y);
+    });
+},
+    reset(){
+        this.position = {
+            x: 100,
+            y: 470
+        };
+    },
+
+    jump(){
+        console.log("jump");
+        if (this.isJumping === true) return;
+    
+        this.isJumping = true;
+        this.position.y -= 110;
+        // this.element.classList.add("jump");
+
+        setTimeout(() => {
+           context.clearRect(this.position.x, this.position.y, 80, 80 );
+          this.position.y += 110;
+          this.isJumping = false;
+          
+          this.loadCat();
+        //   this.element.classList.remove("jump");
+        }, 500);
+      },
+
+    move(direction){
+        console.log("move");
+        context.clearRect(this.position.x, this.position.y, 80, 80 );
+
+
+        if (direction == 'left') {
+            this.position.x -= 4;
+        } else if(direction == 'up'){
+            console.log("up");
+            theCat.jump();
+        } else if(direction == 'down'){
+            this.position.y+=4;
+        } else if(direction == 'right'){
+            this.position.x +=4;
+        }
+
+        theCat.loadCat();
+        requestAnimationFrame(theCat.move);
+    },
+}
 
 
 
- });
+  function handleKeyUp(evt) {
+    console.log(evt.code);
+    if (evt.code === "Space") theCat.jump();
+  }
+  
+  function handleKeyPress(evt) {
+    if (evt.code === "KeyE") theCat.move('up');
+    if (evt.code === "KeyS") {
+        theCat.move('left');
+    }
+    if (evt.code === "KeyF") theCat.move('right');
+  }
+  
+  // event listeners
+  function listen() {
+    window.addEventListener("keypress", handleKeyPress);
+    window.addEventListener("keyup", handleKeyUp);
+  }
+
+
+theCat.loadCat();
+listen();
